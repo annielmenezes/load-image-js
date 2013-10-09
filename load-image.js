d1fed6b566b7b59fd88845c24eefc5a0b1137fab
Anniel Menezes <contato@annielmenezes.com>
2013-10-08 21:11:28 -0300

    Remove duplicate 'use strict' and auto-start some variables

--- load-image.js
+++ load-image.js
@@ -5,52 +5,42 @@
  */
 
 (function(win, doc) {
-
+    // Só é preciso declarar uma vez já está encapsulado na função autoexecultável
+    "use strict"; // JSLINT
     var loadApp = {
-        classSelector: 'load-image'
-        ,index: 0
+        classSelector: 'load-image',
+        index: 0,
 
-        ,init: function(){
-            "use strict"; // JSLINT
-            var images;
-
-            images = doc.getElementsByClassName(this.classSelector);
-
+        init: function(){
+            var images = doc.getElementsByClassName(this.classSelector);
+            console.log(images);
             win.onscroll = function(){
                 loadApp.showImage(images);
-            }
+            };
 
             loadApp.showImage(images);
-        }
+        },
 
-        ,showImage: function(images){
-            "use strict"; // JSLINT
+        showImage: function(images) {
 
             if(images.length !== this.index) {
 
-                var imgPosTop
-                    ,windowPosTop
-                    ,windowHeight;
-
-                imgPosTop    = images[this.index].offsetTop ? images[this.index].offsetTop : 0;
-                windowPosTop = win.pageYOffset;
-                windowHeight = win.innerHeight;
-
-                var isVisible = imgPosTop - windowPosTop < windowHeight;
-
+                var imgPosTop = images[this.index].offsetTop ? images[this.index].offsetTop : 0,
+                    windowPosTop = win.pageYOffset,
+                    windowHeight = win.innerHeight,
+                    // Colocadas aqui por problemas de hoistings (hasteamento)
+                    attrSrc = images[this.index].getAttribute('data-src'),
+                    isVisible = imgPosTop - windowPosTop < windowHeight;
+                    console.log(isVisible);
                 if (isVisible) {
-                    var attrSrc;
+                    images[this.index].setAttribute('src', attrSrc);
+                    images.item(this.index).removeAttribute('data-src');
 
-                    if(attrSrc = images[this.index].getAttribute('data-src')) {
-                        images[this.index].setAttribute('src', attrSrc);
-                        images.item(this.index).removeAttribute('data-src');
-
-                        this.index++;
-                    }
+                    this.index++;
                 }
             }
         }
-    }
+    };
 
     loadApp.init();
 
